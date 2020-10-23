39e75fa1298b0d1610cab0d111cedd968541d41d
thanadolps <thanadolps@gmail.com>
2020-10-23 16:05:44 +0700

    add: theme's primary platte

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#6F516D" },
+  },
   typography: {
     fontFamily: ["Kanit", "Roboto", "sans-serif"].join(","),
   },
